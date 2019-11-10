ec142189b8019b8fb4e075795482eb0f556c7c38
David Egbert <dmegbert@gmail.com>
2019-11-10 10:43:03 -0500

    Create custom sorter function that will not break with non-numbers

--- src/mailer/sendy.py
+++ src/mailer/sendy.py
@@ -25,17 +25,27 @@ def send_condos():
     yag = yagmail.SMTP('python.email.879')
     subject = f'Rincon Condos {date.today()}'
     to = ['dmegbert@gmail.com', 'shanoz617@gmail.com']
-    yag.send('dmegbert@gmail.com', subject, contents)
+    yag.send(to, subject, contents)
 
 
 def make_contents():
     with open('/Users/egbert/projects/condo/data/condos.json', 'r') as f:
         data = json.load(f)
-    data = sorted(data, key=lambda i: int(i.get('bedrooms')), reverse=True)
+
+    data = sorted(data, key=_sorter, reverse=True)
     template_start = f'<h2>Check out {len(data)} possible dream homes!</h2><table><tbody>'
     template_end = '</tbody></table>'
     table_rows = [(get_template(**condo)) for condo in data]
     return [template_start, *table_rows, template_end]
+
+
+def _sorter(element):
+    bedrooms = 0
+    try:
+        bedrooms = int(element.get('bedrooms'))
+    except ValueError:
+        pass
+    return bedrooms
 
 
 if __name__ == '__main__':
